3bd10a647db89d2bc57bc99560fd5b6663441dfb
lulunovii <lulunoviyanti19@gmail.com>
2026-04-17 12:48:41 +0700

    Tugas 2

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-// Import Controller Utama
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\EventController;
+use App\Http\Controllers\EventController as UserEventController;
 use App\Http\Controllers\TicketController;
-// Import Controller di folder Admin
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\EventController as AdminEventController;
 
 // --- HALAMAN USER ---
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/event', [EventController::class, 'show'])->name('event.show');
-Route::get('/checkout', [EventController::class, 'checkout'])->name('checkout');
+Route::get('/event', [UserEventController::class, 'show'])->name('event.show');
+Route::get('/checkout', [UserEventController::class, 'checkout'])->name('checkout');
 Route::get('/ticket', [TicketController::class, 'ticket'])->name('ticket');
 
 // --- HALAMAN ADMIN ---
