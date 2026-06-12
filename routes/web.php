988fac2cbd2a24b89e2da5955eb5c5db1fca9b2d
lulunovii <lulunoviyanti19@gmail.com>
2026-06-12 10:10:09 +0700

    Tugas 8

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EventController as UserEventController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\EventController as EventAdminController;
 use App\Http\Controllers\Admin\CategoryController as CategoryAdminContorller;
@@ -15,11 +16,24 @@ Route::get('/event', [UserEventController::class, 'show'])->name('event.show');
 Route::get('/checkout', [UserEventController::class, 'checkout'])->name('checkout');
 Route::get('/ticket', [TicketController::class, 'ticket'])->name('ticket');
 
+Route::get('/login', function () {
+    return redirect()->route('admin.login');
+})->name('login');
+
 // --- HALAMAN ADMIN ---
+//Grouping untuk URL berawalan \Admin
 Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/transactions', [DashboardController::class, 'transactions'])->name('transactions');
-    Route::resource('events', EventAdminController::class); 
-    Route::resource('categories', CategoryAdminContorller::class);
-    Route::resource('partners', PartnerController::class);
+    // Rute login admin
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    //Mengamankan Route Administrasi di balik tembok (Middleware)
+    Route::middleware(['auth', 'admin'])->group(function () {
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('events', EventAdminController::class);
+        Route::get('transactions', [TransactionController::class, 'transactions'])->name('transactions');
+        Route::resource('categories', CategoryAdminContorller::class);
+        Route::resource('partners', PartnerController::class);
+    });
 });
